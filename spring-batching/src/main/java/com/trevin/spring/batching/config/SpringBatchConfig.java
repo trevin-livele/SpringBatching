33fb759c0b21096e69cb3a64dce0e6a8d846ef27
trevin-livele <liveletrevin6@gmail.com>
2022-12-02 13:18:33 -0500

    added reader and config

--- spring-batching/src/main/java/com/trevin/spring/batching/config/SpringBatchConfig.java
+++ spring-batching/src/main/java/com/trevin/spring/batching/config/SpringBatchConfig.java
@@ -1,10 +1,50 @@
 package com.trevin.spring.batching.config;
 
 
+import com.trevin.spring.batching.entity.Customer;
+import com.trevin.spring.batching.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 @EnableBatchProcessing
+@AllArgsConstructor
+
 public class SpringBatchConfig {
+
+    private JobBuilderFactory jobBuilderFactory;
+
+    private StepBuilderFactory stepBuilderFactory;
+
+    private CustomerRepository repository;
+
+
+    @Bean
+    public FlatFileItemReader<Customer> reader(){
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+
+    }
+
+    private LineMapper<Customer> lineMapper(){
+        DefaultLineMapper<Customer> lineMapper= new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+        delimitedLineTokenizer.setDelimiter("");
+        delimitedLineTokenizer.setStrict(false);
+        delimitedLineTokenizer.setNames("id","firstName","lastName","email","gender","contactNo","country","dob");
+
+    }
 }
